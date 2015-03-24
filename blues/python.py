c67ab5873699165f5bf85c176f83b96d263a3561
Joar Wandborg <joar@wandborg.se>
2015-03-24 15:27:12 +0100

    Add support for Python 3 deploys

--- blues/python.py
+++ blues/python.py
@@ -57,12 +57,21 @@ def install():
 
         req_ver = requested_version()
 
-        if req_ver < (3, 0):
-            debian.apt_get('install', 'python-dev', 'python-setuptools')
-        else:
-            v_milestone = '.'.join(map(str, req_ver[:2]))
-            v_major = str(req_ver[0])
+        # Install python 2.7 easy_install
+        debian.apt_get('install', 'python-setuptools')
 
+        if req_ver < (3, 0):
+            # This assumes < 3.0 means "debian default python".
+            # Python 2.7 is the default on Debian at the moment, so we just
+            # need to install the headers for later use by compiled modules.
+            debian.apt_get('install', 'python-dev')
+        else:
+            # We need to install a non-default python version.
+
+            v_milestone = '.'.join(map(str, req_ver[:2]))  # e.g. "3.4"
+            v_major = str(req_ver[0])  # e.g. "3"
+
+            # Util: easily make python package name.
             def py_pkg(ver, suffix=''):
                 return 'python' + ver + suffix
 
@@ -71,10 +80,14 @@ def install():
                            py_pkg(v_milestone, '-dev'),
                            py_pkg(v_major, '-pip'))
 
+        # Install python default pip.
         run('easy_install pip')
 
+        # Create pip log file
         run('touch {}'.format(pip_log_file))
         debian.chmod(pip_log_file, mode=777)
+
+        # Install latest setuptools via pip, since debian has on old version.
         pip('install', 'setuptools', '--upgrade')
 
 
